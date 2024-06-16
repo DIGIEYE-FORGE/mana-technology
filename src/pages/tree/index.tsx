cdd59ba7f58bfb229fc0ac2709aeb6698df1d98a
issam eljaouhary <iseljao89@gmail.com>
2024-06-16 11:59:42 +0100

    feat: border gradient

--- src/pages/tree/index.tsx
+++ src/pages/tree/index.tsx
@@ -1,3 +1,9 @@
 export default function TreePage() {
-  return <main>TreePage</main>;
+  return (
+    <main className="grid place-content-center">
+      <div className="border-gradient aspect-square w-96 rounded-[24px] p-[4px]">
+        <div className="relative z-10 h-full w-full rounded-[20px] bg-black"></div>
+      </div>
+    </main>
+  );
 }
