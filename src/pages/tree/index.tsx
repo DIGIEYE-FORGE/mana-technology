f852de1ba5e235b015facb04c3340d15b53a5ada
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2024-06-16 16:01:15 +0100

    add page tree

--- src/pages/tree/index.tsx
+++ src/pages/tree/index.tsx
@@ -1,3 +1,26 @@
-export default function TreePage() {
-  return <main>TreePage</main>;
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRef, useEffect } from "react";
+
+const TreePge: React.FC = () => {
+  const canvasRef = useRef(null);
+  const drawCanvas = () => {
+    const canvas: any = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas.getContext("2d");
+    const img = new Image();
+    img.src = "./test.svg";
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      context.drawImage(img, 0, 0);
+    };
+  };
+
+  useEffect(() => {
+    drawCanvas();
+  }, []);
+
+  return <canvas ref={canvasRef} />;
+};
+
+export default TreePge;
