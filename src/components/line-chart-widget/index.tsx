973f484f84f77522b91159e1ead3c6a677972438
issam eljaouhary <iseljao89@gmail.com>
2024-06-16 16:39:46 +0100

    feat: home page

--- src/components/line-chart-widget/index.tsx
+++ src/components/line-chart-widget/index.tsx
@@ -57,86 +57,93 @@ export default function LineChartWidget(props: Props) {
 
   if (isLoading)
     return (
-      <div className="grid h-full w-full place-content-center">
+      <main className="grid place-content-center">
         <Loader />
-      </div>
+      </main>
     );
   if (error)
     return (
-      <div className="grid h-full w-full place-content-center">
+      <main className="grid place-content-center">
         <h3>Something went wrong.</h3>
-      </div>
+      </main>
     );
   return (
-    <ReactApexChart
-      options={{
-        theme: { mode: "dark" },
-        tooltip: { cssClass: "text-black" },
-        colors: telemetries.map((item) => item.color),
-        grid: {
-          borderColor: "#373737",
-          xaxis: { lines: { show: true } },
-          yaxis: { lines: { show: false } },
-        },
-        chart: {
-          background: "transparent",
-          toolbar: { show: false },
-          animations: { enabled: true },
-          zoom: { enabled: false },
-          selection: { enabled: false },
-          dropShadow: { enabled: false },
-        },
-        stroke: { width: 3, curve: "smooth" },
-        dataLabels: { enabled: false },
-        fill: {
-          type: "solid",
-          opacity: telemetries.map((item) => (item.area ? 0.33 : 1)),
-        },
-        legend: {
-          position: "bottom",
-          markers: {
-            width: 26,
-            height: 12,
-          },
-          fontWeight: 600,
-          fontSize: "12px",
-        },
-        xaxis: {
-          type: "datetime",
-          max: dateRange.to ? new Date(dateRange.to).getTime() : undefined,
-          axisBorder: { show: false },
-          axisTicks: { show: false },
-          labels: {
-            show: true,
-            style: {
+    <main className="main flex flex-col">
+      <h1 className="text-center text-lg font-bold text-white">
+        {props.title}
+      </h1>
+      <div className="flex-1">
+        <ReactApexChart
+          options={{
+            theme: { mode: "dark" },
+            tooltip: { cssClass: "text-black" },
+            colors: telemetries.map((item) => item.color),
+            grid: {
+              borderColor: "#373737",
+              xaxis: { lines: { show: true } },
+              yaxis: { lines: { show: false } },
+            },
+            chart: {
+              background: "transparent",
+              toolbar: { show: false },
+              animations: { enabled: true },
+              zoom: { enabled: false },
+              selection: { enabled: false },
+              dropShadow: { enabled: false },
+            },
+            stroke: { width: 3, curve: "smooth" },
+            dataLabels: { enabled: false },
+            fill: {
+              type: "solid",
+              opacity: telemetries.map((item) => (item.area ? 0.33 : 1)),
+            },
+            legend: {
+              position: "bottom",
+              markers: {
+                width: 26,
+                height: 12,
+              },
+              fontWeight: 600,
               fontSize: "12px",
-              fontFamily: "Helvetica, Arial, sans-serif",
-              fontWeight: 400,
-              cssClass: "apexcharts-xaxis-label",
             },
-          },
-        },
-        yaxis: {
-          min: 0,
-          tickAmount: 4,
-          labels: {
-            show: true,
-            formatter: function (value) {
-              return value.toFixed(2);
+            xaxis: {
+              type: "datetime",
+              max: dateRange.to ? new Date(dateRange.to).getTime() : undefined,
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: {
+                show: true,
+                style: {
+                  fontSize: "12px",
+                  fontFamily: "Helvetica, Arial, sans-serif",
+                  fontWeight: 400,
+                  cssClass: "apexcharts-xaxis-label",
+                },
+              },
             },
-            style: {
-              fontSize: "12px",
-              fontFamily: "Helvetica, Arial, sans-serif",
-              fontWeight: 400,
-              cssClass: "apexcharts-xaxis-label",
+            yaxis: {
+              min: 0,
+              tickAmount: 4,
+              labels: {
+                show: true,
+                formatter: function (value) {
+                  return value.toFixed(2);
+                },
+                style: {
+                  fontSize: "12px",
+                  fontFamily: "Helvetica, Arial, sans-serif",
+                  fontWeight: 400,
+                  cssClass: "apexcharts-xaxis-label",
+                },
+              },
             },
-          },
-        },
-      }}
-      series={data || []}
-      // type={"area"}
-      width={"100%"}
-      height={"100%"}
-    />
+          }}
+          series={data || []}
+          // type={"area"}
+          width={"100%"}
+          height={"100%"}
+        />
+      </div>
+    </main>
   );
 }
