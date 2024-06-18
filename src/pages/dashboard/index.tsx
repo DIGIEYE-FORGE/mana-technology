54e5eb06cba467c9590909a03a6a0cf77887c2b6
issam eljaouhary <iseljao89@gmail.com>
2024-06-18 14:29:51 +0100

    update global colors

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/card";
 
 export default function DashboardPage() {
   return (
-    <main className="grid w-full grid-flow-dense auto-rows-[76px] grid-cols-9 gap-3">
+    <main className="grid w-full grid-flow-dense auto-rows-[96px] grid-cols-9 gap-3">
       <Card className="col-span-2 row-span-2"></Card>
       <Card className="col-span-4 row-span-9"></Card>
       <Card className="col-span-3 row-span-5"></Card>
