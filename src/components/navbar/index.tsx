7d46ad2e8d441a5a69cd02f0a78664eeb4d9fe0e
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2024-06-20 17:23:21 +0100

    add navbar range

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,9 @@ import { Button } from "../ui/button";
 import { useAppContext } from "@/Context";
 import { LogOut } from "lucide-react";
 import { cn } from "@/lib/utils";
-
+import { DatePickerWithRange } from "../calander";
+import React from "react";
+import { DateRange } from "react-day-picker";
 const links = [
   { name: "Home", path: "/" },
   { name: "Tree", path: "/tree" },
@@ -15,11 +17,20 @@ export default function Navbar() {
     useAppContext();
 
   const { pathname } = useLocation();
-
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(),
+    to: new Date(),
+  });
   return (
     <nav className="sticky top-0 z-10 mx-auto flex items-center justify-between gap-6 divide-background px-4 py-4 lg:px-6">
       <div className="hidden border lg:inline-block">
-        date range selector goes here
+        <DatePickerWithRange
+          className="w-full"
+          date={date}
+          onChange={(date) => {
+            setDate(date as DateRange);
+          }}
+        />
       </div>
       <div className="flex">
         {links.map((link) => (
