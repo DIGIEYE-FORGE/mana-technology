f09ef435498dc779eb41a99197d8571ef623171a
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2024-06-25 11:42:17 +0100

    fixed

--- src/components/models/index.tsx
+++ src/components/models/index.tsx
@@ -1,25 +1,67 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // src/Model.tsx
-import { forwardRef, useEffect } from "react";
-import { useLoader } from "@react-three/fiber";
+import { forwardRef } from "react";
+import { useLoader, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as THREE from "three";
+import React from "react";
+
+void useThree;
+const vertexShader = `
+varying vec3 vPosition;
+
+void main() {
+  vPosition = position;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
+
+const fragmentShader = `
+uniform vec3 color1;
+uniform vec3 color2;
+varying vec3 vPosition;
+
+void main() {
+  float gradient = (vPosition.y + 2.0) / 2.0; // Normalize y to range [0, 1]
+  gl_FragColor = vec4(mix(color1, color2, gradient), 2.0);
+}
+`;
 
 const Model = forwardRef(
-  ({ url, color }: { url: string; color: string }, ref: any) => {
+  (
+    {
+      url,
+      color1,
+      color2,
+      onLoad,
+    }: { url: string; color1: string; color2: string; onLoad: () => void },
+    ref: any,
+  ) => {
     const gltf = useLoader(GLTFLoader, url);
+    const shaderMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        color1: { value: new THREE.Color(color1) },
+        color2: { value: new THREE.Color(color2) },
+      },
+      vertexShader,
+      fragmentShader,
+    });
 
-    useEffect(() => {
-      gltf.scene.traverse((node) => {
-        if ((node as any).isMesh) {
-          (node as any).castShadow = true;
-          (node as any).receiveShadow = true;
+    React.useEffect(() => {
+      if (gltf) {
+        onLoad();
+      }
+    }, [gltf, onLoad]);
 
-          // add color to the mesh
-          (node as any).material.color.set(color);
-        }
-      });
-    }, [gltf]);
+    gltf.scene.traverse((node) => {
+      if ((node as any).isMesh) {
+        (node as any).castShadow = true;
+        (node as any).receiveShadow = true;
 
+        (node as any).material = shaderMaterial;
+      }
+    });
+    // return <primitive object={gltf.scene} ref={ref} />;
     return <primitive object={gltf.scene} ref={ref} />;
   },
 );
