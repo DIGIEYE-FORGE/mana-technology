9000a6f4000c01a711342559121ac94f6cd30b46
issam eljaouhary <iseljao89@gmail.com>
2024-06-25 12:29:25 +0100

    fix: updae underground upbar back button

--- src/pages/underground/index.tsx
+++ src/pages/underground/index.tsx
@@ -1,19 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { MoveLeftIcon } from "lucide-react";
 import { useAppContext } from "@/Context";
 import { DatePickerWithRange } from "@/components/calander";
 import { TDateRange } from "@/utils";
+import { cn } from "@/lib/utils";
 
 function UndergroundBar() {
   const { dateRange, setDateRange } = useAppContext();
+  const { pathname } = useLocation();
+
+  const parentPath = pathname.split("/").slice(0, -1).join("/");
 
   return (
     <div className="h-up-bar group sticky top-0 z-10 flex w-[calc(10)] shrink-0 items-center gap-4 rounded-[0_0_24px_0] border-b border-r px-6 backdrop-blur">
-      <Link to="/main-project">
+      <Link to={parentPath || "/main-project"}>
         <Button variant="ghost">
-          <MoveLeftIcon className="size-6" />
+          <MoveLeftIcon
+            size={24}
+            className={cn("transition-transform duration-500", {
+              "rotate-90": parentPath,
+            })}
+          />
         </Button>
       </Link>
       <DatePickerWithRange
