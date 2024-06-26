99e5c2c3b069a5e5e5d8920541dc9d3faef6ea86
issam eljaouhary <iseljao89@gmail.com>
2024-06-26 11:42:32 +0100

    feat: updata model position

---
+++ src/pages/tree/components/dashboard-02/index.tsx
@@ -0,0 +1,15 @@
+import { Card } from "@/components/card";
+
+export const Dashboard2 = () => {
+  return (
+    <main className="grid grid-cols-4 gap-4">
+      <Card className="col-span-2">1</Card>
+      <Card className="col-span-2">2</Card>
+      <Card className="col-span-2">3</Card>
+      <Card className="">4</Card>
+      <Card className="">5</Card>
+      <Card className="col-span-2">6</Card>
+      <Card className="col-span-2">7</Card>
+    </main>
+  );
+};
