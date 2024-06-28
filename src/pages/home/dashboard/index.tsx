05d2d76823de6ec0a8345e30c8248cd10acf7918
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2024-06-28 11:50:22 +0100

    add op

--- src/pages/home/dashboard/index.tsx
+++ src/pages/home/dashboard/index.tsx
@@ -3,15 +3,12 @@ import { ProgressAccumulation } from "../_components/progress-accumulation";
 import TableWidget from "@/components/table-widget";
 import LineChartWidget from "@/components/line-chart-widget";
 import BarChartWidget from "@/components/bar-chart-widget";
-import MultiProgressWidget from "@/components/multi-progress-widget";
 import ProgressMultiple from "@/components/progress-multiple";
-import { format } from "date-fns";
-import { CalendarDays } from "lucide-react";
 
 export default function HomeDashboard() {
   return (
-    <main className="grid w-full grid-flow-dense auto-rows-[80px] grid-cols-3 gap-3 sm:auto-rows-[92px] md:grid-cols-6 lg:grid-cols-12 lg:gap-4 2xl:grid-cols-[repeat(15,minmax(0,1fr))]">
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-6 2xl:col-span-5">
+    <div className="grid h-full w-full grid-flow-dense auto-rows-[19rem] grid-cols-9 gap-6">
+      <Card className="col-span-3 flex flex-col p-4">
         <h1 className="text-center text-lg font-semibold">
           Évolution Production Cumulée
         </h1>
@@ -23,14 +20,14 @@ export default function HomeDashboard() {
                   area: false,
                   name: "EST_PLANIFIE_ROCHE_CUMUL",
                   color: "#78F6EA",
-                  label: "Cumulative planifié",
+                  label: "Cumulatif Planifié",
                   serial: "U9XQMQ1DXYT7LJIP",
                 },
                 {
                   area: true,
                   name: "EST_REALISE_ROCHE_CUMUL_Ton",
                   color: "#B98EFF",
-                  label: "Cumulative realisé",
+                  label: "Cumulatif Realisé",
                   serial: "U9XQMQ1DXYT7LJIP",
                 },
               ],
@@ -38,13 +35,12 @@ export default function HomeDashboard() {
           />
         </div>
       </Card>
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-6 2xl:col-span-5">
+      <Card className="col-span-3 flex flex-col p-4">
         <h1 className="text-center text-lg font-semibold">
-          Production par Fosse
+          Production Journalière
         </h1>
         <div className="flex-1">
           <BarChartWidget
-            moyenne={["EST11_REALISE_ROCHE", "EST12_REALISE_ROCHE"]}
             attributes={{
               stacked: true,
               telemetries: [
@@ -55,20 +51,13 @@ export default function HomeDashboard() {
                   label: "Est 11",
                   serial: "U9XQMQ1DXYT7LJIP",
                 },
-                {
-                  name: "EST12_REALISE_ROCHE",
-                  unit: "T",
-                  color: "#B98EFF",
-                  label: "Est 12",
-                  serial: "U9XQMQ1DXYT7LJIP",
-                },
               ],
             }}
           />
         </div>
       </Card>
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-6 2xl:col-span-5">
-        <h1 className="text-center text-lg font-semibold">Stérile / Minera</h1>
+      <Card className="col-span-3 flex flex-col p-4">
+        <h1 className="text-center text-lg font-semibold">Stérile / Minerai</h1>
         <div className="flex-1">
           <BarChartWidget
             attributes={{
@@ -106,49 +95,21 @@ export default function HomeDashboard() {
           />
         </div>
       </Card>
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-6 2xl:col-span-4">
+      <Card className="col-span-2 flex flex-col p-4">
         <h1 className="text-center text-lg font-semibold">
           Évolution de la Production vs Planifié
         </h1>
         <ProgressAccumulation
           attributes={{
             serial: "U9XQMQ1DXYT7LJIP",
-            progressColor: "#FF5AF1",
-            currentTargetColor: "#727DC6",
+            progressColor: "#EBC94A",
+            currentTargetColor: "#78F6EA",
             progressTelemetryName: "EST_REALISE_ROCHE_CUMUL_Ton",
             accumulationTelemetryName: "EST_PLANIFIE_ROCHE_CUMUL",
           }}
         />
       </Card>
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-6 2xl:col-span-4">
-        <h1 className="text-center text-lg font-semibold">
-          Production journalière
-        </h1>
-        <div className="flex-1">
-          <BarChartWidget
-            attributes={{
-              stacked: false,
-              telemetries: [
-                {
-                  name: "EST_PLANIFIE_ROCHE",
-                  unit: "T",
-                  color: "#78F6EA",
-                  label: "Planifié",
-                  serial: "U9XQMQ1DXYT7LJIP",
-                },
-                {
-                  name: "EST_REALISE_ROCHE_Ton",
-                  unit: "T",
-                  color: "#B98EFF",
-                  label: "Realisé",
-                  serial: "U9XQMQ1DXYT7LJIP",
-                },
-              ],
-            }}
-          />
-        </div>
-      </Card>
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-6 2xl:col-span-4">
+      <Card className="col-span-4 flex flex-col p-4">
         <h1 className="text-center text-lg font-semibold">
           Production par qualité
         </h1>
@@ -159,20 +120,21 @@ export default function HomeDashboard() {
                 title: "Sulfures (HG,MG,LG)",
                 telemetries: [
                   {
-                    name: "EST_REALISE_ORE_HG_SULF",
-                    serial: "U9XQMQ1DXYT7LJIP",
+                    name: "SUD4_REALISEORE_HG_SULF",
+                    serial: "C6XPYU0D920L1M07",
                     label: "HG",
                     color: "#FFDC8C",
+                    // showLabel: true, if you need to show label
                   },
                   {
-                    name: "EST_REALISE_ORE_MG_SULF",
-                    serial: "U9XQMQ1DXYT7LJIP",
+                    name: "SUD4_REALISE_ORE_MG_SULF",
+                    serial: "C6XPYU0D920L1M07",
                     label: "MG",
                     color: "#42F5FF",
                   },
                   {
-                    name: "EST_REALISE_ORE_LG_SULF",
-                    serial: "U9XQMQ1DXYT7LJIP",
+                    name: "SUD4_REALISE_ORE_LG_SULF",
+                    serial: "C6XPYU0D920L1M07",
                     label: "LG",
                     color: "#FFC8FF",
                   },
@@ -182,20 +144,20 @@ export default function HomeDashboard() {
                 title: "Oxydes (HG,MG,LG)",
                 telemetries: [
                   {
-                    name: "EST_REALISE_ORE_HG_OXYD",
-                    serial: "U9XQMQ1DXYT7LJIP",
+                    name: "SUD4_REALISE_ORE_HG_OXYD",
+                    serial: "C6XPYU0D920L1M07",
                     label: "HG",
                     color: "#FFDC8C",
                   },
                   {
-                    name: "EST_REALISE_ORE_MG_OXYD",
-                    serial: "U9XQMQ1DXYT7LJIP",
+                    name: "SUD4_REALISE_ORE_MG_OXYD",
+                    serial: "C6XPYU0D920L1M07",
                     label: "MG",
                     color: "#42F5FF",
                   },
                   {
-                    name: "EST_REALISE_ORE_LG_OXYD",
-                    serial: "U9XQMQ1DXYT7LJIP",
+                    name: "SUD4_REALISE_ORE_LG_OXYD",
+                    serial: "C6XPYU0D920L1M07",
                     label: "LG",
                     color: "#FFC8FF",
                   },
@@ -205,14 +167,15 @@ export default function HomeDashboard() {
           />
         </div>
       </Card>
-      <Card className="col-span-full row-span-3 flex flex-col gap-1 p-4 lg:col-span-8">
-        <h3 className="text-center text-lg font-semibold">
-          Disponibilité Engine
-        </h3>
+      <Card className="col-span-3 row-span-2 flex items-center justify-center overflow-hidden text-3xl font-bold">
+        <img src="/est.png" alt="Logo" className="h-full w-full object-cover" />
+      </Card>
+      <Card className="col-span-6 flex flex-col p-4">
+        <h3 className="text-center text-lg font-semibold">Suivi des Tires</h3>
         <TableWidget
           className="h-1 flex-1"
           attributes={{
-            serial: "AATT1JKEZ2V1YVMA",
+            serial: "C6XPYU0D920L1M07",
             element: "telemetries",
             mappings: [
               {
@@ -247,151 +210,6 @@ export default function HomeDashboard() {
           }}
         />
       </Card>
-      <Card className="col-span-full row-span-3 flex flex-col p-6 lg:col-span-4">
-        <h1 className="text-center text-lg font-semibold">
-          Evolution Production cumule
-        </h1>
-        <div className="flex-1">
-          <LineChartWidget
-            moyenne={"combined"}
-            attributes={{
-              telemetries: [
-                {
-                  area: true,
-                  name: "EST_REALISE_FORATION",
-                  color: "#B98EFF",
-                  label: "Realisé ML/J",
-                  serial: "U9XQMQ1DXYT7LJIP",
-                },
-                {
-                  area: false,
-                  name: "EST_PLANIFIE_FORATION",
-                  color: "#78F6EA",
-                  label: "Objectif ML/J",
-                  serial: "U9XQMQ1DXYT7LJIP",
-                },
-              ],
-            }}
-          />
-        </div>
-      </Card>
-      <Card className="col-span-full row-span-3 flex flex-col gap-3 p-4 2xl:col-span-3">
-        <h3 className="text-center text-lg font-semibold">
-          Disponibilité Engine GMC
-        </h3>
-        <MultiProgressWidget
-          attributes={{
-            telemetries: [
-              {
-                name: "GMC_FOREUSE_EPRIROC_T45_01_DISPO",
-                color: "#ecc94b",
-                label: "Foreuse Epiroc  T45 (1)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_FOREUSE_EPRIROC_T45_02_DISPO",
-                color: "#ecc94b",
-                label: "Foreuse Epiroc  T45 (2)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_FOREUSE_EPRIROC_T35_01_DISPO",
-                color: "#ecc94b",
-                label: "Foreuse Epiroc T35 ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_PELLE_CAT374_01_DISPO",
-                color: "#ecc94b",
-                label: "Pelle CAT 374 (1)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_PELLE_CAT374_02_DISPO",
-                color: "#ecc94b",
-                label: "Pelle CAT 374 (2) ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_PELLE_CAT374_03_DISPO",
-                color: "#ecc94b",
-                label: "Pelle CAT 374 (3)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_PELLE_CAT350_01_DISPO",
-                color: "#ecc94b",
-                label: "Pelle CAT 350 (1)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_PELLE_CAT350_02_DISPO",
-                color: "#ecc94b",
-                label: "Pelle CAT 350",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_PELLE_CAT_0000_01",
-                color: "#ecc94b",
-                label: " Pelle CAT ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_CAMIONS_SCANIA50T_GROUPE",
-                color: "#ecc94b",
-                label: "Camions Scania 50t (15)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_DOZER_D8_01",
-                color: "#ecc94b",
-                label: "D8 ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_DOZER_D8_02",
-                color: "#ecc94b",
-                label: "D8 (2)",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_NIVELEUSE_01",
-                color: "#ecc94b",
-                label: "Niveleuse ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_COMPACTEUR_01",
-                color: "#ecc94b",
-                label: "Compacteur ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-              {
-                name: "GMC_CITERNE_01",
-                color: "#ecc94b",
-                label: "Citerne d'arrosage ",
-                serial: "U9XQMQ1DXYT7LJIP",
-              },
-            ],
-          }}
-        />
-      </Card>
-      <Card className="col-span-full row-span-3 flex flex-col gap-3 p-4 2xl:col-span-3">
-        <h3 className="text-center text-lg font-semibold">Comment</h3>
-        <div className="flex items-center gap-2">
-          <CalendarDays size={20} />
-          {format(new Date(), "PP p")}
-        </div>
-        <div className="h-1 flex-1 overflow-auto">
-          <q className="text-center">
-            {" "}
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde
-            distinctio, sed eos consequuntur sequi magnam nam quasi impedit
-            error perferendis rerum ducimus possimus perspiciatis voluptate
-            minima! Possimus adipiscialsdjlkajlkasddjlksajlkdj sdlfkjsdf dslkj{" "}
-          </q>
-        </div>
-      </Card>
-    </main>
+    </div>
   );
 }
