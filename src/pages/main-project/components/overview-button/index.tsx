56fc5f2d7436d6639be59f142a2d212cec04b0b3
issam eljaouhary <iseljao89@gmail.com>
2024-07-03 00:41:23 +0100

    feat: update project overview dialog content

--- src/pages/main-project/components/overview-button/index.tsx
+++ src/pages/main-project/components/overview-button/index.tsx
@@ -2,17 +2,50 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 import { DialogClose } from "@radix-ui/react-dialog";
-import { XIcon } from "lucide-react";
+import { MoveLeft, MoveRight, XIcon } from "lucide-react";
+import { useState } from "react";
+
+const tabs = [
+  {
+    title: "title 1",
+    subTabs: [
+      {
+        title: "sub title 1",
+        image: "/image1.svg",
+      },
+    ],
+  },
+  {
+    title: "title 2",
+    subTabs: [
+      {
+        title: "sub title 2",
+        image: "/image2.svg",
+      },
+    ],
+  },
+  {
+    title: "title 3",
+    subTabs: [
+      {
+        title: "sub title 3",
+        image: "/image3.svg",
+      },
+    ],
+  },
+];
 
 interface OverviewButtonProps extends React.HTMLAttributes<HTMLButtonElement> {}
 
 export function OverviewButton({ className, ...props }: OverviewButtonProps) {
+  const [activeTab, setActiveTab] = useState(0);
+  const [activeSubTab, setActiveSubTab] = useState(0);
   return (
     <Dialog>
       <DialogTrigger asChild>
         <button
           className={cn(
-            "rounded-lg bg-white/10 px-3 py-1.5 text-primary active:bg-white/20",
+            "rounded-lg border-current bg-white/10 px-3 py-1.5 text-[#9cc6e3] hover:brightness-110 active:brightness-90",
             className,
           )}
           {...props}
@@ -24,15 +57,67 @@ export function OverviewButton({ className, ...props }: OverviewButtonProps) {
         overlayClassName="bg-transparent"
       >
         <div className="relative mx-auto flex w-full max-w-[1920px] gap-2">
-          <DialogClose>
-            <Button
-              variant={"ghost"}
-              size={"icon"}
-              className="absolute right-2 top-2"
+          {tabs.map((tab, index) => (
+            <button
+              key={index}
+              className={cn(
+                "custom-button flex flex-1 justify-center rounded-lg py-2 text-xl font-bold uppercase transition-colors hover:brightness-125",
+                {
+                  "bg-[#00A3FF]/50 hover:brightness-110": activeTab === index,
+                },
+              )}
+              onClick={() => {
+                setActiveTab(index);
+                setActiveSubTab(0);
+              }}
             >
-              <XIcon size={20} />
-            </Button>
+              {tab.title}
+            </button>
+          ))}
+          <DialogClose asChild>
+            <button
+              className={cn(
+                "custom-button flex select-none justify-center rounded-lg bg-[#3A3D3F]/75 px-6 py-2 text-xl font-bold uppercase transition-colors hover:brightness-105",
+              )}
+            >
+              <XIcon className="h-6 w-6" />
+            </button>
           </DialogClose>
+        </div>
+        <div
+          className="mx-auto flex h-1 max-h-[920px] w-full max-w-[1920px] flex-1 flex-col gap-4"
+          style={{
+            backgroundImage: "url(/public/dashboard-frame.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% 100%",
+          }}
+        >
+          <div className="flex h-20 justify-between">
+            <div className="mb-5 ml-16 flex items-center">
+              <span className="text-2xl font-bold first-letter:uppercase">
+                {tabs[activeTab].subTabs[activeSubTab].title}
+              </span>
+            </div>
+            <div className="mr-4 mt-5 flex w-[62%] items-center px-6">
+              {activeSubTab > 0 && (
+                <Button
+                  variant={"ghost"}
+                  onClick={() => setActiveSubTab(activeSubTab - 1)}
+                >
+                  <MoveLeft className="h-6 w-6" />
+                </Button>
+              )}
+              {activeSubTab < tabs[activeTab].subTabs.length - 1 && (
+                <Button
+                  variant={"ghost"}
+                  className="ml-auto"
+                  onClick={() => setActiveSubTab(activeSubTab + 1)}
+                >
+                  <MoveRight className="h-6 w-6" />
+                </Button>
+              )}
+            </div>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
