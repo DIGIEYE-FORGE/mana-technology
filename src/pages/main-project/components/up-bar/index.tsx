79d2a9446a6d4a2d27c98bdbcfc8cbe0006033de
issam eljaouhary <iseljao89@gmail.com>
2024-07-03 00:13:43 +0100

    feat: add logo to main project upbar

--- src/pages/main-project/components/up-bar/index.tsx
+++ src/pages/main-project/components/up-bar/index.tsx
@@ -1,19 +1,30 @@
 import { Button } from "@/components/ui/button";
-import { MoveLeftIcon, MoveRightIcon } from "lucide-react";
+import { FilePieChart, MoveLeftIcon, MoveRightIcon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { OverviewButton } from "../overview-button";
 
 export function MainProjectUpBar() {
   return (
     <div className="group sticky top-0 z-10 flex h-up-bar w-full shrink-0 items-center justify-between gap-4 border-b px-6 backdrop-blur">
-      <Link
-        to="/"
-        className="opacity-0 transition-opacity duration-500 group-hover:opacity-100"
-      >
-        <Button variant="ghost">
-          <MoveLeftIcon className="size-6" />
-        </Button>
-      </Link>
-      <div className="text-2xl font-medium">Tizert Mine Project Overview</div>
+      <div className="flex items-center gap-2">
+        <Link
+          to="/"
+          className="opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+        >
+          <Button variant="ghost">
+            <MoveLeftIcon className="size-6" />
+          </Button>
+        </Link>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <div className="group relative flex items-center gap-2 text-2xl font-medium">
+        <span className="h-full py-3">Tizert Mine</span>
+
+        <OverviewButton className="absolute right-1/2 top-1/2 flex translate-x-1/2 items-center gap-2 whitespace-nowrap opacity-0 transition-[top,opacity] group-hover:top-full group-hover:opacity-100">
+          <FilePieChart size={20} />
+          Project Overview
+        </OverviewButton>
+      </div>
       <Link
         to="/underground"
         className="opacity-0 transition-opacity duration-500 group-hover:opacity-100"
