f88fd6f6e5d0465b05221ca1df1f8283d7717a12
ichoukri <ichoukri1337@gmail.com>
2024-07-03 12:50:12 +0100

    update: charts data

--- src/pages/main-project/components/hse-button/data.ts
+++ src/pages/main-project/components/hse-button/data.ts
@@ -1,3 +1,5 @@
+import { add, addDays } from "date-fns";
+
 export const widgetsData = {
   0: {
     // title: "Chiffre effectif (Cost Value)",
@@ -5,18 +7,21 @@ export const widgetsData = {
     telemetryName: "HSE_MP_Total",
     label: "Disponibilité",
     serial: "XEXX5PDMNHBWUN7Q",
+    value: 105,
   },
   1: {
     title: "Cumul des heures travaillées",
     telemetryName: "HSE_MH_Total",
     label: "Disponibilité",
     serial: "XEXX5PDMNHBWUN7Q",
+    value: 2607521.5,
   },
   2: {
     title: "Taux do clôture",
     telemetryName: "MECANISE_TIZERT_JUMBO_M20_PERC_DISPO",
     label: "Disponibilité",
     serial: "XEXX5PDMNHBWUN7Q",
+    value: 90.5,
   },
   3: {
     title: "HSE Project Indicators",
@@ -60,6 +65,40 @@ export const widgetsData = {
           color: "#78F6EA",
           label: "Heures Travaillées",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
       ],
     },
@@ -73,6 +112,40 @@ export const widgetsData = {
           color: "#FF5AF1",
           label: "Fréquence",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
       ],
     },
@@ -86,21 +159,21 @@ export const widgetsData = {
           color: "#25A18E",
           label: "Injury",
           serial: "U9XQMQ1DXYT7LJIP",
-          value: 10,
+          value: 12,
         },
         {
           name: "EST_REALISE_ROCHE_CUMUL_Ton",
           color: "#FF5AF1",
           label: "Asset Damage",
           serial: "U9XQMQ1DXYT7LJIP",
-          value: 20,
+          value: 37,
         },
         {
           name: "EST_REALISE_ROCHE_CUMUL_Ton",
           color: "#D2DDFF",
           label: "Environment",
           serial: "U9XQMQ1DXYT7LJIP",
-          value: 30,
+          value: 6,
         },
       ],
     },
@@ -115,12 +188,80 @@ export const widgetsData = {
           color: "#FF5AF1",
           label: "Asset",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
         {
           name: "HSE_ENVI_Total",
           color: "#D2DDFF",
           label: "Environment",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
       ],
     },
@@ -135,30 +276,200 @@ export const widgetsData = {
           color: "#25A18E",
           label: "ONS",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
         {
           name: "HSE_Incident_OFS",
           color: "#25A18E",
           label: "OFS",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
         {
           name: "HSE_Incident_UG",
           color: "#25A18E",
           label: "ug",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
         {
           name: "HSE_Incident_OP",
           color: "#25A18E",
           label: "OP",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
         {
           name: "HSE_Incident_OP",
           color: "#25A18E",
           label: "OP",
           serial: "XEXX5PDMNHBWUN7Q",
+          data: [
+            {
+              x: addDays(new Date(), -7),
+              y: 100,
+            },
+            {
+              x: addDays(new Date(), -6),
+              y: 105,
+            },
+            {
+              x: addDays(new Date(), -5),
+              y: 110,
+            },
+            {
+              x: addDays(new Date(), -4),
+              y: 115,
+            },
+            {
+              x: addDays(new Date(), -3),
+              y: 120,
+            },
+            {
+              x: addDays(new Date(), -2),
+              y: 125,
+            },
+            {
+              x: addDays(new Date(), -1),
+              y: 130,
+            },
+            {
+              x: new Date(),
+              y: 135,
+            },
+          ]
         },
       ],
     },
