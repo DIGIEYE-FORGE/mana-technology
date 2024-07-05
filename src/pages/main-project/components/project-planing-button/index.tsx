c3975d5e34a136acc2630d60dfd33d3a0a015eda
issam eljaouhary <iseljao89@gmail.com>
2024-07-05 12:58:00 +0100

    feat: update svg tab

--- src/pages/main-project/components/project-planing-button/index.tsx
+++ src/pages/main-project/components/project-planing-button/index.tsx
@@ -74,14 +74,51 @@ const tabs = [
     ],
   },
   {
-    title: "ESG",
+    title: "esg ",
     subTabs: [
       {
-        title: "hse",
-        image: "",
+        title: "cost report",
+        image: "/esg-01.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-02.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-03.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-04.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-05.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-06.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-07.svg",
+      },
+      {
+        title: "Earned Value Management",
+        image: "/esg-08.png",
       },
     ],
   },
+  // {
+  //   title: "ESG",
+  //   subTabs: [
+  //     {
+  //       title: "hse",
+  //       image: "",
+  //     },
+  //   ],
+  // },
 ];
 
 interface ProjectPlaningButtonProps
@@ -166,17 +203,11 @@ function ProjectPlaningButton({
             </div>
           </div>
           <div className="mb-8 ml-16 mr-8 h-1 flex-1">
-            {tabs[activeTab].subTabs[activeSubTab].title === "hse" ? (
-              <div className="h-full w-full">
-                <HseButton />
-              </div>
-            ) : (
-              <img
-                src={tabs[activeTab].subTabs[activeSubTab].image}
-                alt={tabs[activeTab].subTabs[activeSubTab].title}
-                className="h-full w-full"
-              />
-            )}
+            <img
+              src={tabs[activeTab].subTabs[activeSubTab].image}
+              alt={tabs[activeTab].subTabs[activeSubTab].title}
+              className="h-full w-full"
+            />
           </div>
         </div>
       </DialogContent>
