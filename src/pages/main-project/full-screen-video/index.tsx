bd09b4d40fe5a74999966c7fcb5ddbaf674c22f3
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2024-07-10 14:51:59 +0100

    fixed

--- src/pages/main-project/full-screen-video/index.tsx
+++ src/pages/main-project/full-screen-video/index.tsx
@@ -4,8 +4,8 @@ const FullscreenVideo = ({ src }: { src: string }) => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (videoRef.current) {
-      (videoRef?.current as any)?.requestFullscreen().catch((err: any) => {
+    if (videoRef?.current) {
+      (videoRef?.current as any)?.requestFullscreen()?.catch((err: any) => {
         console.error("Error attempting to enable full-screen mode:", err);
       });
     }
