f64e7eb7e99062eceeaf81767117ea5e0fd6c08d
ichoukri <ichoukri1337@gmail.com>
2024-07-10 20:19:41 +0100

    add: mobile qr code

---
+++ src/pages/main-project/components/qr-code-modal/index.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { ScanQrCode } from "lucide-react";
+
+export const QrCodeDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"ghost"}>
+          <ScanQrCode />
+        </Button>
+      </DialogTrigger>
+      <DialogContent
+        showCloseButton={false}
+        className="rounded-lg border-none p-6 text-white"
+      >
+        <img src="/mobile-qr-code.svg" alt="qr-code" className="rounded-lg" />
+        <div className="px-3 pb-3">
+          <DialogClose className="h-10 w-full rounded-lg bg-primary px-4 text-lg font-semibold text-primary-foreground hover:bg-primary/90">
+            Cancel
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
