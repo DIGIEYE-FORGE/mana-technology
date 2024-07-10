d58168a323ec6eafe46d2470a60da968efe1ad02
issam eljaouhary <iseljao89@gmail.com>
2024-07-10 15:37:12 +0100

    fix: add subtabs to planning model

--- src/pages/mobile/components/project-planing-button/index.tsx
+++ src/pages/mobile/components/project-planing-button/index.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 import { DialogClose } from "@radix-ui/react-dialog";
-import { XIcon } from "lucide-react";
+import { MoveLeft, MoveRight, XIcon } from "lucide-react";
 import { useState } from "react";
 
 const tabs = [
@@ -98,7 +98,7 @@ function ProjectPlaningButton({
       </DialogTrigger>
       <DialogContent
         showCloseButton={false}
-        className="max dark bottom-0 top-[4rem] z-[102] flex w-[100%] max-w-none translate-y-0 flex-col rounded-none border-none bg-card/10 p-4 text-foreground backdrop-blur-xl"
+        className="dark bottom-0 top-[4rem] z-[102] flex w-[100%] max-w-none translate-y-0 flex-col rounded-none border-none bg-card/10 p-4 text-foreground backdrop-blur-xl"
         overlayClassName="bg-transparent"
       >
         <div className="hide-scrollbar relative mx-auto flex w-full max-w-[1920px] gap-2 overflow-x-auto">
@@ -130,14 +130,63 @@ function ProjectPlaningButton({
           </DialogClose>
         </div>
         <div
-          className="relative mx-auto flex h-1 max-h-[13rem] w-full max-w-[1920px] flex-1 flex-col gap-4 2xl:max-h-[920px]"
+          className="relative mx-auto flex h-1 max-h-[16rem] w-full max-w-[1920px] flex-1 flex-col gap-4 2xl:max-h-[920px]"
           style={{
             backgroundImage: "url(/dashboard-frame.png)",
             backgroundRepeat: "no-repeat",
             backgroundSize: "100% 100%",
           }}
         >
-          <div className="ml-3 mr-1 mt-8 h-1 flex-1 sm:ml-4 sm:mr-4 sm:mt-10 md:ml-6 md:mr-6 lg:mb-8 lg:ml-16 lg:mr-8 lg:mt-20">
+          <div className="hidden h-20 justify-between lg:flex">
+            <div className="mb-5 ml-16 flex items-center">
+              <span className="text-2xl font-bold first-letter:uppercase">
+                {tabs[activeTab].subTabs[activeSubTab].title}
+              </span>
+            </div>
+            <div className="mr-4 mt-5 flex w-[62%] items-center justify-end gap-4 px-6">
+              {activeSubTab > 0 && (
+                <Button
+                  variant={"ghost"}
+                  className="mr-auto"
+                  size={"icon"}
+                  onClick={() => setActiveSubTab(activeSubTab - 1)}
+                >
+                  <MoveLeft className="h-6 w-6" />
+                </Button>
+              )}
+              {activeSubTab < tabs[activeTab].subTabs.length - 1 && (
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  onClick={() => setActiveSubTab(activeSubTab + 1)}
+                >
+                  <MoveRight className="h-6 w-6" />
+                </Button>
+              )}
+            </div>
+          </div>
+          <div className="relative ml-3 mr-1 mt-8 h-1 flex-1 sm:ml-4 sm:mr-4 sm:mt-10 md:ml-6 md:mr-6 lg:mb-8 lg:ml-16 lg:mr-8 lg:mt-20">
+            {activeSubTab > 0 && (
+              <Button
+                variant={"ghost"}
+                className="absolute bottom-full right-1/2 translate-y-1/2 lg:hidden"
+                size={"icon"}
+                onClick={() => setActiveSubTab(activeSubTab - 1)}
+              >
+                <MoveLeft className="size-4" />
+              </Button>
+            )}
+            {activeSubTab < tabs[activeTab].subTabs.length - 1 && (
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                onClick={() => setActiveSubTab(activeSubTab + 1)}
+                className="absolute bottom-full right-0 translate-y-1/2 lg:hidden"
+              >
+                <MoveRight className="size-4" />
+              </Button>
+            )}
+
             <img
               src={tabs[activeTab].subTabs[activeSubTab].image}
               alt={tabs[activeTab].subTabs[activeSubTab].title}
