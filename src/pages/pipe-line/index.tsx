bc67df18acbce27316e663a384a592a280be6f44
drabi-he <drabi-he@Digieye>
2025-05-27 22:48:56 +0100

    tweaks

--- src/pages/pipe-line/index.tsx
+++ src/pages/pipe-line/index.tsx
@@ -523,7 +523,7 @@ const PipeLine: React.FC = () => {
         where: {
           serial: "JHF455XKPCH6DBLH",
           createdAt: {
-            $gt: new Date(Date.now() - 3 * 60 * 60 * 1000),
+            $gt: new Date(Date.now() - 1 * 60 * 60 * 1000),
             // Last 24 hours
           },
         },
