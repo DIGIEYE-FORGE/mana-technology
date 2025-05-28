2367234e13721be2ffb1be3028a63c13bbb23dc8
salama <salama.yabahddou@um5r.ac.ma>
2025-05-28 17:44:48 +0100

    feat: add units for pipeline attributes in PipelinePoint component

--- src/pages/pipe-line/index.tsx
+++ src/pages/pipe-line/index.tsx
@@ -24,6 +24,7 @@ import { useAppContext } from "@/Context";
 import useSWR from "swr";
 import { env } from "@/utils/env";
 import { io } from "socket.io-client";
+
 interface Position {
   top?: string;
   bottom?: string;
@@ -78,6 +79,16 @@ interface PipelinePointProps {
   showCard: boolean;
   onPointClick: (id: string) => void;
 }
+
+const ATTRIBUTE_UNITS: Record<string, string> = {
+  "Chlore input": "(mg/L)",
+  "Chlore output": "(mg/L)",
+  "Flow input": "(L/s)",
+  "Flow output": "(L/s)",
+  "delta flow": "(L/s)",
+  "pression output": "(bar)",
+  "Pression": "(bar)",
+};
 
 const createPositionStyle = (position: Position): React.CSSProperties => {
   return { ...position };
@@ -219,7 +230,9 @@ const PipelinePoint: React.FC<PipelinePointProps> = ({
               <div className="flex flex-1 flex-col gap-1">
                 {Object.entries(point.card.attributes)?.map(([key, value]) => (
                   <div key={key} className="flex justify-between">
-                    <span className="text-xs text-gray-400">{key}</span>
+                    <span className="text-xs text-gray-400">
+                      {key} {ATTRIBUTE_UNITS[key] ? ATTRIBUTE_UNITS[key] : ""}
+                    </span>
                     <span className="text-xs text-white">
                       {key == "Running state" ? (
                         <div className="flex flex-wrap gap-1">
